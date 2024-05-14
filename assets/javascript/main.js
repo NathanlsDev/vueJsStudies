823a026985812e3cbed632881e9ffaf09fe1571d
Nathan Lima <nathanls.dev@gmail.com>
2024-05-13 22:24:56 -0300

    attribute binding

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,6 +2,10 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    description: "Vue Socks: +5 comfy code."
+    description: "Vue Socks: +5 comfy code.",
+    image:
+      "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+      altText: "A pair of green socks",
+    link: "https://www.amazon.com/s?k=green+socks&ref=cs_503_search",
   },
 });
